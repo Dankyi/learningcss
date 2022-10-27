551185979c0bbc842d0c8402282504212ed75f19
Dankyi <dankyiboateng15@gmail.com>
2022-10-27 19:40:58 +0100

    tailwind-proj: compiled/build tailwindcss into a css file and configured screen sizes and colors

--- tailwind-proj/tailwind.config.js
+++ tailwind-proj/tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976",
+			xl: "1440px"
+		},
+		extend: {
+			colors: {
+				lightRed: "hsl(12, 88%, 60%)",
+				lighterRed: "hsl(12, 90%, 70%)",
+				whitishRed: "hsl(12, 88%, 95%)",
+				snowRed: "hsl(13, 100%, 96%)",
+				darkBlue: "hsl(228, 40%, 24%)",
+				darkGray: "hsl(232, 12%, 14%)",
+				lightGray: "hsl(227, 12%, 60%)",
+				lighterGray: "hsl(0, 0%, 98%)",
+			}
+		},
 	},
 	plugins: [],
 }
