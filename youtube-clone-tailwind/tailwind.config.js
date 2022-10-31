9b71705a7f261b641f58ceaa2a52ac683178dd4e
Dankyi <dankyiboateng15@gmail.com>
2022-10-31 16:25:46 +0000

    youtube-clone-tailwind: created the header

--- youtube-clone-tailwind/tailwind.config.js
+++ youtube-clone-tailwind/tailwind.config.js
@@ -11,8 +11,10 @@ module.exports = {
 		colors: {
 			"darkGray": "#606060",
 			"darkerGray": "#4c4c4c",
-			"lightGray": "#f0f0f0",
-			"lighterGray": "#f8f8f8"
+			"lightGray": "#9b9a9a",
+			"semiLightGray": "#f2efef",
+			"lighterGray": "#f0f0f0",
+			"lightestGray": "#f8f8f8"
 		},
 		fontFamily: {
 			roboto: ['"Roboto"', 'Arial'],
