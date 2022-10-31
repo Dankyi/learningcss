ce2b5b37099df9f57a84be4cee70ff7746034be1
Dankyi <dankyiboateng15@gmail.com>
2022-10-31 13:31:19 +0000

    youtube-clone-tailwind: customized screen sizes, colors and font family

--- youtube-clone-tailwind/tailwind.config.js
+++ youtube-clone-tailwind/tailwind.config.js
@@ -1,7 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		colors: {
+			"darkGray": "#606060",
+			"darkerGray": "#4c4c4c",
+			"lightGray": "#f0f0f0",
+			"lighterGray": "#f8f8f8"
+		},
+		fontFamily: {
+			roboto: ['"Roboto"', 'Arial'],
+		},
 		extend: {},
 	},
 	plugins: [],
