4b9134c9efcd0b3fe71f662f83f1fd9abef1140d
Dankyi <dankyiboateng15@gmail.com>
2022-11-01 16:30:02 +0000

    removed tailwind-proj

--- tailwind-proj/js/script.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.getElementById("menu-btn");
-const nav = document.getElementById("menu");
-
-menuBtn.addEventListener("click", () => {
-	menuBtn.classList.toggle("open");
-	nav.classList.toggle("flex");
-	nav.classList.toggle("hidden");
-});
